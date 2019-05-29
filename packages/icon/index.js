5f494913922b34d8e7404182ff49d7d2bcbe6c7c
shiwan <shiwanwan@thinkjoy.cn>
2019-05-29 20:09:30 +0800

    just use dist

--- packages/icon/index.js
+++
@@ -1,7 +0,0 @@
-import WIcon from './src/icon.vue';
-
-WIcon.install = function (Vue) {
-  Vue.component(WIcon.name, WIcon);
-};
-
-export default WIcon;
--- packages/loading/index.js
+++
@@ -1,7 +0,0 @@
-import WLoading from './src/loading.vue';
-
-WLoading.install = function (Vue) {
-  Vue.component(WLoading.name, WLoading);
-};
-
-export default WLoading;
