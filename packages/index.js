bc375bf031aadf5add5ce5b0805873635f9f6ba2
vannvan <862670198@qq.com>
2020-12-03 11:35:38 +0800

    fix:解决modal组件mask禁用点击关闭不生效的问题

--- packages/index.js
+++ packages/index.js
@@ -1,14 +1,14 @@
 // Copyright (c) 2019 by https://github.com/vannvan.
 // All Rights Reserved.
 
-import WButton from './button/index.js';
-import WModal from './modal/index.js';
-import WAlert from './alert/index';
-import WIcon from './icon/index';
-import WMessage from './message/index';
-import WNotice from './notice/index';
-import WLoading from './loading/index';
-import WMenu from './menu/index'
+import WButton from "./button/index.js";
+import WModal from "./modal/index.js";
+import WAlert from "./alert/index";
+import WIcon from "./icon/index";
+import WMessage from "./message/index";
+import WNotice from "./notice/index";
+import WLoading from "./loading/index";
+import WMenu from "./menu/index";
 const components = [
   WButton,
   WModal,
@@ -17,19 +17,29 @@ const components = [
   WNotice,
   WIcon,
   WLoading,
-  WMenu
-]
+  WMenu,
+];
 
-const install = function(Vue) {
-  if (install.installed) return
-  components.map(component => Vue.component(component.name, component))
+const install = function (Vue) {
+  if (install.installed) return;
+  components.map((component) => Vue.component(component.name, component));
   Vue.prototype.$Modal = WModal;
   Vue.prototype.$Message = WMessage;
   Vue.prototype.$Notice = WNotice;
-}
+  Vue.prototype.$WUI = {
+    transfer: "transfer" in opts ? opts.transfer : "",
+    modal: {
+      maskClosable: opts.modal
+        ? "maskClosable" in opts.modal
+          ? opts.modal.maskClosable
+          : ""
+        : "",
+    },
+  };
+};
 
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
 }
 
 export default {
@@ -41,5 +51,5 @@ export default {
   WNotice,
   WIcon,
   WLoading,
-  WMenu
-}
+  WMenu,
+};
--- packages/modal/src/modal.vue
+++ packages/modal/src/modal.vue
@@ -1,86 +1,117 @@
 <template>
   <div>
     <transition name="fade">
-      <div class="public-mask" v-show="visible">
-      </div>
+      <div class="public-mask" v-show="visible"></div>
     </transition>
     <transition name="modal-fade">
       <div class="modal-wrap" v-if="visible" @click="handleMask()">
-          <div class="public-modal clearfix"  :style="modalWidth" v-show="visible" @click.stop>
-            <div :class="[prefixCls + '--header']" v-if="showHead">
-              <slot name="header">{{title}}</slot>
-              <i class="icon-close" :class="[prefixCls + '__closeIcon']" @click="close()" v-if="closable"></i>
+        <div
+          class="public-modal clearfix"
+          :style="modalWidth"
+          v-show="visible"
+          @click.stop
+        >
+          <div :class="[prefixCls + '--header']" v-if="showHead">
+            <slot name="header">{{ title }}</slot>
+            <i
+              class="icon-close"
+              :class="[prefixCls + '__closeIcon']"
+              @click="close()"
+              v-if="closable"
+            ></i>
+          </div>
+          <div :class="[prefixCls + '--content']">
+            <slot></slot>
+          </div>
+          <div
+            class="modal-footer"
+            :class="[prefixCls + '--footer']"
+            v-if="!footerHide"
+          >
+            <div
+              :class="[prefixCls + '--btn', prefixCls + '--btn--cancel']"
+              @click="close()"
+            >
+              取消
             </div>
-            <div :class="[prefixCls + '--content']">
-              <slot></slot>
-            </div>
-            <div class="modal-footer" :class="[prefixCls + '--footer']" v-if="!footerHide">
-              <div :class="[prefixCls + '--btn',prefixCls + '--btn--cancel']" @click="close()">取消</div>
-              <div :class="[prefixCls + '--btn',prefixCls + '--btn--ok',prefixCls + '--btn--' + type]" @click="ok()">{{okText}}</div>
+            <div
+              :class="[
+                prefixCls + '--btn',
+                prefixCls + '--btn--ok',
+                prefixCls + '--btn--' + type,
+              ]"
+              @click="ok()"
+            >
+              {{ okText }}
             </div>
           </div>
+        </div>
       </div>
     </transition>
-
   </div>
-
 </template>
 
 <script>
-const prefixCls = 'wui__modal';
+const prefixCls = "wui__modal";
 export default {
-  name:'WModal',
-  data(){
-    return{
+  name: "WModal",
+  data() {
+    return {
       prefixCls: prefixCls,
-      visible:this.value,
-      showHead:true,
-    }
+      visible: this.value,
+      showHead: true,
+    };
   },
   props: {
     type: {
       type: String,
-      default: 'default'
+      default: "default",
     },
     width: {
       type: String,
-      default:'400'
+      default: "400",
     },
     value: {
-       type: Boolean,
-       default: false
+      type: Boolean,
+      default: false,
     },
     title: {
       type: String,
     },
-    okText:{
-      type:String,
-      default:'确定'
+    okText: {
+      type: String,
+      default: "确定",
     },
     content: {
       type: String,
-      default: '这是弹框内容'
+      default: "这是弹框内容",
     },
     footerHide: {
       type: Boolean,
-      default: false
+      default: false,
     },
     closable: {
       type: Boolean,
-      default:false
-    }
-  },
-  watch:{
-    value (val) {
-        this.visible = val;
+      default: false,
+    },
+    maskClosable: {
+      type: Boolean,
+      default() {
+        return !this.$WUI || this.$WUI.transfer === "" ? true : false;
+      },
     },
   },
-  computed:{
-    modalWidth(){
-      const style = {}
-      style.width = this.width +'px';
-      return style
-    }
+  watch: {
+    value(val) {
+      this.visible = val;
+    },
+  },
+  computed: {
+    modalWidth() {
+      const style = {};
+      style.width = this.width + "px";
+      return style;
+    },
   },
   mounted() {
     //do something after mounting vue instance
@@ -88,52 +119,51 @@ export default {
     // console.log(this.footerHide)
     let showHead = true;
     if (this.$slots.header == undefined && !this.title) {
-        showHead = false;
+      showHead = false;
     }
     this.showHead = showHead;
   },
   methods: {
-    close(){
-      this.visible = false
-      this.$emit('input', false);  //this is important
+    close() {
+      this.visible = false;
+      this.$emit("input", false); //this is important
     },
     handleMask() {
       // console.log('clicked mask')
-      this.visible = false
-      this.$emit('input', false);  //this is important
+      if (this.maskClosable) {
+        this.close();
+      }
     },
-    ok(){
-      this.$emit('on-ok');
-      this.visible = false
-      this.$emit('input', false);  //this is important
+    ok() {
+      this.$emit("on-ok");
+      this.visible = false;
+      this.$emit("input", false); //this is important
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss">
-.modal-wrap{
-    position: fixed;
-    overflow: auto;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1000;
+.modal-wrap {
+  position: fixed;
+  overflow: auto;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1000;
+  outline: 0;
+  .public-modal {
+    margin: 0 auto;
+    position: relative;
     outline: 0;
-    .public-modal{
-      margin: 0 auto;
-      position: relative;
-      outline: 0;
-      top: 100px;
-      background: #fff;
-      .modal-footer{
-        height:  35px;
-        width: 320px;
-        margin:25px auto;
-      }
+    top: 100px;
+    background: #fff;
+    .modal-footer {
+      height: 35px;
+      width: 320px;
+      margin: 25px auto;
     }
+  }
 }
-
-
 </style>
